beeab9dbe4b0819827d9c0ab77e411a267fdf018
xiaomage <1191971402@qq.com>
2017-10-15 23:21:46 +0800

    Add code

---
+++ 代码/spring-boot-jdbc/src/main/java/com/gupao/springbootjdbc/SpringBootJdbcApplication.java
@@ -0,0 +1,12 @@
+package com.gupao.springbootjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcApplication.class, args);
+	}
+}
