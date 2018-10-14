c9f9cdfad5ad41213e5969dbcb66ca97eb6778a3
gouyan <2637178209@qq.com>
2018-10-14 16:00:40 +0800

    gx

--- VIP课/microservices/source/microservices-project/spring-cloud-project/spring-cloud-native-application/src/main/java/com/gupao/micro/services/spring/cloud/SpringBootApplicationBootstrap.java
+++ VIP课/microservices/source/microservices-project/spring-cloud-project/spring-cloud-native-application/src/main/java/com/gupao/micro/services/spring/cloud/SpringBootApplicationBootstrap.java
@@ -4,14 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableAutoConfiguration
 @RestController
+@SpringBootApplication
 public class SpringBootApplicationBootstrap {
 
     public static void main(String[] args) {
@@ -26,7 +30,7 @@ public class SpringBootApplicationBootstrap {
 
         // 类比于 Spring WebMVC，Root WebApplication 和 DispatcherServlet WebApplication
         // DispatcherServlet WebApplication parent = Root WebApplication
-        // DispatcherServlet Servlet
+        // DispatcherServlet 是一个Servlet，那么Filter想用里面的bean怎么办？弄一个父Application作为通用bean交集
         // Filter -> Root WebApplication
 
         new SpringApplicationBuilder(SpringBootApplicationBootstrap.class)
@@ -42,5 +46,22 @@ public class SpringBootApplicationBootstrap {
     public String index() {
         return message;
     }
-
+//    public static void main(String[] args) {
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+//        context.refresh();
+//        context.setId("小马哥");    // 设置上下文名称
+//        // 向小马哥上下文中 注册bean，beanName为"helloWorld"，然后当前上下文直接可以使用@Autowired String helloWorld 获取其bean了
+//        context.registerBean("helloWorld",String.class,"HelloWorld");
+//        new SpringApplicationBuilder(SpringBootApplicationBootstrap.class)
+//                .parent(context)   // 显示设置双亲上下文
+//                .run(args);
+//    }
+//    @Autowired
+//    @Qualifier("helloWorld")
+//    private String helloWorld;  // 使用父上下文小马哥 中的 bean
+//
+//    @RequestMapping(value = "/",method = RequestMethod.GET)
+//    public String index(){
+//        return helloWorld;
+//    }
 }
