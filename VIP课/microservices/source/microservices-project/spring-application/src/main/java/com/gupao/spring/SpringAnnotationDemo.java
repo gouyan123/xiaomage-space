f98551d8ac62439ab832c588bdcfcb95c6ee0d0c
gouyan <2637178209@qq.com>
2018-10-14 22:21:37 +0800

    gx

--- VIP课/microservices/source/microservices-project/spring-application/src/main/java/com/gupao/spring/SpringAnnotationDemo.java
+++ VIP课/microservices/source/microservices-project/spring-application/src/main/java/com/gupao/spring/SpringAnnotationDemo.java
@@ -8,10 +8,9 @@ public class SpringAnnotationDemo {
 
     public static void main(String[] args) {
 
-        //   XML 配置文件驱动       ClassPathXmlApplicationContext
-        // Annotation 驱动
-        // 找 BeanDefinition
-        // @Bean @Configuration
+        // XML配置文件驱动上下文 ClassPathXmlApplicationContext
+        // Annotation注解驱动上下文
+        // 两者都是找 BeanDefinition的，即@Bean或者@Configuration注释的类的；
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         // 注册一个 Configuration Class = SpringAnnotationDemo
         context.register(SpringAnnotationDemo.class);
